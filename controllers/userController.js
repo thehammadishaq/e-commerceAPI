1740f04d5db80bfdbb421d2d6d614f6afee68a14
thehammadishaq <thehammadishaq@gmail.com>
2024-06-30 23:35:49 +0500

    User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const getAllUsers = async (req, res) => {
+    res.send('getAllUsers')
+}
+const getSingleUser = async (req, res) => {
+    res.send('getSingleUser')
+}
+const showCurrentUser = async (req, res) => {
+    res.send('showCurrentUser')
+}
+const updateUser = async (req, res) => {
+    res.send('updateUser')
+}
+const updateUserPassword = async (req, res) => {
+    res.send('updateUserPassword')
+}
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+}
