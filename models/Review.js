6437cac5085406f599b2eac10b650cdd3037fb48
thehammadishaq <thehammadishaq@gmail.com>
2024-07-03 23:55:38 +0500

    Review Model

---
+++ models/Review.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+
+    },
+    title: {
+        type: String,
+        required: [true, 'please provide title'],
+        maxlength: [50, 'title cannot be more than 50 characters']
+
+
+    },
+    comment: {
+        type: String,
+
+        required: [true, 'please provide comment'],
+        maxlength: [500, 'comment cannot be more than 500 characters']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: true
+    }
+
+}, { timestamps: true })
