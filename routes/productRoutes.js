10d0d5b07369914dbaee0b0aee75d652ccbe03d4
thehammadishaq <thehammadishaq@gmail.com>
2024-07-03 23:30:09 +0500

    Product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,4 +5,20 @@ const { authenticateUser, authorizeUser } = require('../middleware/authenticatio
 const { createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct, uploadImage } = require('../controllers/productController');
 
 
-router.route('/')
+router
+    .route('/')
+    .get(getAllProducts)
+    .post([authenticateUser, authorizePermissions('admin')], createProduct)
+
+router
+    .route('/uploadImage')
+    .post([authenticateUser, authorizePermissions('admin')], uploadImage)
+
+router
+    .route('/:id')
+    .put([authenticateUser, authorizePermissions('admin')], updateProduct)
+    .delete([authenticateUser, authorizePermissions('admin')], deleteProduct)
+    .get(getSingleProduct)
+
+
+module.exports = router;
